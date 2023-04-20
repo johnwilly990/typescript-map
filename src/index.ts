c5ed2931c285b0440913cef82174ee6440fbc6e3
johnwilly990 <j.kim1940@gmail.com>
2023-04-19 21:29:36 -0400

    Add google maps api script and its related dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
+/// <reference types="@types/google.maps" />
 import { User } from "./classes/User";
 import { Company } from "./classes/Company";
 
 const user = new User();
 const company = new Company();
-
-console.log(company);
