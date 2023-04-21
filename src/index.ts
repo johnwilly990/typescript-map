c88e36589a010fe53ce0795c80458f1e41127b0b
johnwilly990 <j.kim1940@gmail.com>
2023-04-21 15:04:56 -0400

    Create map class using dynamic div id

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 /// <reference types="@types/google.maps" />
 import { User } from "./classes/User";
 import { Company } from "./classes/Company";
+import { Map } from "./classes/Map";
 
 const user = new User();
 const company = new Company();
+new Map("map");
