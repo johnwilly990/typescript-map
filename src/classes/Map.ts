fc724eb8b50810be20051fc330d2eb9c13b6dd5d
johnwilly990 <j.kim1940@gmail.com>
2023-04-21 15:51:30 -0400

    Add marker of user and company with popup window

--- src/classes/Map.ts
+++ src/classes/Map.ts
@@ -1,3 +1,11 @@
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
 export class Map {
   private googleMap: google.maps.Map;
 
@@ -9,5 +17,23 @@ export class Map {
         center: { lat: 0, lng: 0 },
       }
     );
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,7 @@ import { Map } from "./classes/Map";
 
 const user = new User();
 const company = new Company();
-new Map("map");
+const map = new Map("map");
+
+map.addMarker(user);
+map.addMarker(company);
